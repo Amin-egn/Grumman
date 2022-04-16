53ec966cc847d5530c60e727aa6d693e402e3555
amin_egn <aminx15am@yahoo.com>
2022-04-16 16:48:29 +0430

    add BaseView to panel and raw model'

--- panel/views.py
+++ panel/views.py
@@ -1,4 +1,14 @@
+from django.views import View
 from django.shortcuts import render
+
+
+class BaseView(View):
+    """Base View"""
+    TEMPLATE = None
+
+    def render(self, request, template=None, **kwargs):
+        template = template or self.TEMPLATE
+        return render(request, template, kwargs)
 
 
 def home_view(request):
